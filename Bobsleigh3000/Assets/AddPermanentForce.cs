0c39115baaba72b8a7c519f8b7d4a09fef5ca672
detienne1 <bazook@justbeatemup.com>
2020-02-07 17:50:52 +0100

    -added trucs

---
+++ Bobsleigh3000/Assets/AddPermanentForce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPermanentForce : MonoBehaviour
+{
+    public float force = 2f;
+    public enum Axe{
+        axeX,
+        axeY,
+        axeZ
+    }
+
+    public Axe axe = Axe.axeX;
+    Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion inverseRotation = Quaternion.Euler(-1f * transform.rotation.eulerAngles.x, -1f * transform.rotation.eulerAngles.y, -1f * transform.rotation.eulerAngles.z);
+        _rigidbody.AddRelativeForce(inverseRotation * transform.forward * force);
+    }
+}
