45a3b8595102a88a8ca5f1dc88be5bdc771bf6cf
videti <dev.detienne@gmail.com>
2020-02-10 14:00:24 +0100

    - fix lookat player

--- Bobsleigh3000/Assets/Scripts/FollowingBezierCurve.cs
+++ Bobsleigh3000/Assets/Scripts/FollowingBezierCurve.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class FollowingBezierCurve : MonoBehaviour
 {
-    [Range(0, 3f)]
+    [Range(-1, 3f)]
     public float speed = 1f;
     [Range(10f, 30f)]
     public float totalTime = 30f;
     [Range(0f, 360f)]
     public float angle = 40f;
-    [Range(0f, 1f)]
+    [Range(0f, 1.5f)]
     public float radius = 1f;
+
+    public float targetDistance = 0.05f;
 
     public Vector3 localCamPos = new Vector3(0, 0.8f, -0.8f);
 
@@ -26,7 +28,6 @@ public class FollowingBezierCurve : MonoBehaviour
     private void Start()
     {
         nextOrigin = Vector3.zero;
-        radius = 1f - GetComponentInChildren<Collider>().bounds.size.y * 0.7f;
     }
 
     private void OnEnable()
@@ -54,13 +55,17 @@ public class FollowingBezierCurve : MonoBehaviour
         Vector3 newPosition = new Vector3(radius * Mathf.Cos(angle * Mathf.Deg2Rad), radius * Mathf.Sin(angle * Mathf.Deg2Rad), 0);
         newPosition = Quaternion.LookRotation(dir) * newPosition;
         newPosition += origin;
+
+        Vector3 farOrigin = BezierShape.GetBezierCurvePointAtT(scriptableControl.ctrlPoints, timeBuffer + 0.05f);
+
         //on modifie le transform
-        transform.LookAt(newPosition);
+        //transform.LookAt(BezierShape.GetBezierCurvePointAtT(scriptableControl.ctrlPoints, timeBuffer + targetDistance));
+        transform.LookAt(transform.position + dir * 10f);
+        //transform.LookAt(newPosition);
         transform.position = newPosition;
         transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, angle + 90f);
 
         //on replace la caméra
-        Vector3 farOrigin = BezierShape.GetBezierCurvePointAtT(scriptableControl.ctrlPoints, timeBuffer + 0.05f);
         thirdPersonCam.transform.position = origin + thirdPersonCam.transform.rotation * localCamPos;
         thirdPersonCam.transform.LookAt(farOrigin + thirdPersonCam.transform.rotation * localCamPos);
         thirdPersonCam.transform.Rotate(30f, 0, 0);
